7608e212001395c070d7a4f93f1d300c49324e8b
HeiRica <heitorm50@gmail.com>
2025-10-17 16:43:03 -0300

    Refactor code for clarity and conciseness

--- Códigos/Questao2.c
+++ Códigos/Questao2.c
@@ -6,44 +6,33 @@
 */
 #include <stdio.h>
 
-// Função MDC (máximo divisor comum)
 int mdc(int a, int b) {
-    while (b != 0) {                         // Loop até b ser 0        
-        int t = b;                           // Swap       
-        b = a % b;              
+    while (b != 0) {
+        int t = b;
+        b = a % b;
         a = t;
     }
     return a;
 }
 
-// Função MMC (mínimo múltiplo comum)
 int mmc(int a, int b) {
-    return (a * b) / mdc(a, b);             // MMC(a, b) = (a*b)/MDC(a,b)
+    return (a * b) / mdc(a, b);
 }
 
 int main() {
-    int N, i, ciclos[10];
-    int resultado;
+    int N, ciclos[10], resultado;
 
-    printf("Digite o número de chaves: ");
     scanf("%d", &N);
+    for (int i = 0; i < N; i++) scanf("%d", &ciclos[i]);
 
-    printf("Digite os ciclos das chaves: ");
-    for (i = 0; i < N; i++) {
-        scanf("%d", &ciclos[i]);
-    }
-
-    // MMC acumulado
     resultado = ciclos[0];
-    for (i = 1; i < N; i++) {
-        resultado = mmc(resultado, ciclos[i]);  // Atualiza resultado com o MMC atual       
-    }
+    for (int i = 1; i < N; i++)
+        resultado = mmc(resultado, ciclos[i]);
 
-    if (resultado > 50) {
-        printf("Não é possível sincronizar todas as chaves dentro do limite de 50 anos.\n");
-    } else {
+    if (resultado > 50)
+        printf("Nao e possivel sincronizar todas as chaves dentro do limite.\n");
+    else
         printf("%d\n", resultado);
-    }
 
     return 0;
 }
